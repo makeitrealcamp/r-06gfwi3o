cbedb5fa1d3e979ca626c3babe6446a3cf0d2086
German Escobar <german.escobarc@gmail.com>
2016-11-29 16:22:33 -0500

    Prepare challenge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      names: ["Juan", "Pedro", "Germán"]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.names.map((name) => {
+          <Welcome name={name} />
+        })}
       </div>
     );
   }
